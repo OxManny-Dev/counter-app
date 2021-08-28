60a2d8a9d2104c976c1e7a0d3368f5fb383289c5
Emmanuel <emmanueljucaban@gmail.com>
2021-08-28 11:02:08 -0500

    finish todo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import { useState } from 'react';
-import {
-  Button,
-  ButtonGroup,
-  Grid,
-  makeStyles,
-} from '@material-ui/core';
+import { Button, ButtonGroup, Grid } from '@material-ui/core';
 import ViewUno from './ViewOne';
 import ViewTwo from './ViewTwo';
-
-const useStyles = makeStyles(() => ({
-  coloredRed: {
-    color: 'magenta',
-    fontSize: '32px',
-  },
-  coloredGreen: {
-    color: 'green',
-    fontSize: '72px',
-  },
-}));
-
 const App = () => {
-  const classes = useStyles();
   const [ currentPage, setCurrentPage ] = useState(1);
   return (
-    <Grid container spacing={2} >
+    <Grid container spacing={2}>
       <Grid item sm={12}>
         <ButtonGroup disableElevation variant="contained" color="primary">
           <Button
@@ -38,7 +20,7 @@ const App = () => {
           </Button>
         </ButtonGroup>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item sm={12}>
         {
           currentPage === 1 ?
             <ViewUno/>
@@ -65,16 +47,3 @@ const App = () => {
   );
 }
 export default App;
-// <div>
-//   <button
-//     onClick={ () => setCurrentPage(1)}
-//   >
-//     Change View One
-//   </button>
-//   <button
-//     onClick={ () => setCurrentPage(2)}
-//   >
-//     Change View Two
-//   </button>
-
-// </div>
--- src/ViewOne.js
+++ src/ViewOne.js
@@ -1,16 +1,42 @@
 import { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
+import { Button, Grid } from '@material-ui/core';
 export const ViewOne = () => {
   const [ todoText, setTodoText ] = useState('');
-  console.log(todoText);
+  const [ todos, setTodos ] = useState([]);
   return (
     <>
       <TextField
+        fullWidth
         label='Please enter your email address here'
         variant='outlined'
         value={todoText}
         onChange={(e) => setTodoText(e.target.value)}
       />
+      <Button
+        color='primary'
+        variant='contained'
+        fullWidth
+        onClick={() => {
+          const newTodos = [ ...todos, todoText ];
+          setTodos(newTodos);
+          setTodoText('');
+        }}
+      >
+        Add todo
+      </Button>
+      <Grid container>
+        <Grid item xs={12}>
+          {
+            todos.length === 0 ?
+              <h1>No todos yet</h1>
+              :
+              todos.map(todo => {
+                return <h1>{todo}</h1>
+              })
+          }
+        </Grid>
+      </Grid>
     </>
   )
 };
