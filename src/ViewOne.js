78c5a988efa287995b8e29c9152c6e448e0552b7
Emmanuel <emmanueljucaban@gmail.com>
2021-08-30 19:33:08 -0500

    add axios and useEffect

--- src/ViewOne.js
+++ src/ViewOne.js
@@ -1,14 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect, } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { Button, Grid } from '@material-ui/core';
+import axios from 'axios';
 export const ViewOne = () => {
   const [ todoText, setTodoText ] = useState('');
   const [ todos, setTodos ] = useState([]);
+  //1st param is function to call after we return html
+  // and it's only called 1 time
+  //2nd param is an array of elements to listen for changes for
+  // Whenever a value inside of the array is changed, the 1st parameter which the function will be called again
+  // Component will unmount
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+        // console.log(response.data);
+        setTodos(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+    // console.log('I AM INSIDE OF USE EFFECT');
+    // When component leaves the page, this function will be called
+    // return () => {
+    //   console.log('I AM LEAVING!!!');
+    // }
+  }, [  ]);
+  console.log('i am rendering');
   return (
     <>
       <TextField
         fullWidth
-        label='Please enter your email address here'
+        label='Please add a todo'
         variant='outlined'
         value={todoText}
         onChange={(e) => setTodoText(e.target.value)}
@@ -32,7 +54,7 @@ export const ViewOne = () => {
               <h1>No todos yet</h1>
               :
               todos.map(todo => {
-                return <h1>{todo}</h1>
+                return <h1>{todo.title}</h1>
               })
           }
         </Grid>
